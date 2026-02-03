eab8ac157db9a3b3e2ad5c372785076d925cfc8a
przemek <przemek.bialkwno@gmail.com>
2026-02-03 19:15:30 +0100

    adding supabase - connecting

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,11 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
+
+  console.log(cabins);
 
   return (
     <div>
