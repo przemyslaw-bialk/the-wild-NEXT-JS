ce8ee4a0d7d508fbc692ddca2ad108b497355573
przemek <przemek.bialkwno@gmail.com>
2026-02-08 09:51:24 +0100

    dynamic routes, error handling

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import supabase from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -11,11 +12,8 @@ export async function getCabin(id) {
     .eq("id", id)
     .single();
 
-  // For testing
-  //
-
   if (error) {
-    console.error(error);
+    notFound();
   }
 
   return data;
