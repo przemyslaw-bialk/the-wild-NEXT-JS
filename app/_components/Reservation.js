1dc0e77a6563eefd97f8a792bdb4ea04665570c4
przemek <przemek.bialkwno@gmail.com>
2026-02-13 14:29:00 +0100

    getting user session

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,10 +1,14 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 const Reservation = async ({ cabin }) => {
   const settings = await getSettings();
   const bookedDates = await getBookedDatesByCabinId(cabin.id);
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -14,7 +18,15 @@ const Reservation = async ({ cabin }) => {
         cabin={cabin}
       />
 
-      <ReservationForm settings={settings} cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm
+          settings={settings}
+          cabin={cabin}
+          user={session.user}
+        />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 };
