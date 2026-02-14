fb7228d193469f1c1444a0a6725cae691ae0816a
przemek <przemek.bialkwno@gmail.com>
2026-02-14 17:44:05 +0100

    singin, signout functionality, setting middleware

---
+++ app/_lib/actions.js
@@ -0,0 +1,11 @@
+"use server";
+
+import { signIn, signOut } from "./auth";
+
+export async function signinAction() {
+  await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signoutAction() {
+  await signOut({ redirectTo: "/" });
+}
--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,13 @@
-export default function Page() {
+import { auth } from "../_lib/auth";
+
+export default async function Page() {
+  const session = await auth();
+  console.log(session);
+
+  if (!session) return <div>Not authenticated</div>;
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Przemek
+      Welcome, {session.user.name}
     </h2>
   );
 }
