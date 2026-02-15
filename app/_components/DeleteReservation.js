9532e6d292038c23af3831ec5df49b10fdebdb0e
przemek <przemek.bialkwno@gmail.com>
2026-02-15 19:47:31 +0100

    server actions: updating a booking

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,15 +1,32 @@
 "use client";
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteReservation } from "../_lib/actions";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
+  const [isPending, startTransision] = useTransition();
+
+  function handleDelete() {
+    if (confirm("are u sure u want to delete this reservation?"))
+      startTransision(() => deleteReservation(bookingId));
+  }
+
   return (
     <button
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDelete}
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
